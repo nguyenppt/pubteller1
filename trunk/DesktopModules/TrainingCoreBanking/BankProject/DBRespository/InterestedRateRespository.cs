138dc3f8da55d7e88be190ae2d4647aac71fbe13
usr.nghiale@gmail.com <usr.nghiale@gmail.com@611a926c-3eee-aedd-d1d3-a3589350bd61>
2014-09-21 13:30:22 +0000

    User to get Interested Rate data

---
+++ trunk/DesktopModules/TrainingCoreBanking/BankProject/DBRespository/InterestedRateRespository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BankProject.DBContext;
+
+namespace BankProject.DBRespository
+{
+    public class InterestedRateRespository:BaseRepository<BINTEREST_RATE>
+    {
+
+    }
+}
